07844d38baad692f3f08cce1b6f7c018af6ef3ef
David Ellis <d.f.ellis@ieee.org>
2013-06-20 09:41:16 -0700

    Simplified the jscoverage code. Hopefully upstreams accepts this change.

--- test/test.js
+++ test/test.js
@@ -151,34 +151,10 @@ exports.onlyConfirmOnceFailPath2 = function(test) {
 exports.jscoverage = function(test) {
     test.expect(3);
     jscoverage.coverageDetail();
-    // Copied directly from jscoverage and edited, since getting at these values directly isn't possible
-    var file;
-    var tmp;
-    var total;
-    var touched;
-    var n, len;
-    if (typeof global._$jscoverage === 'undefined') {
-        return;
-    }
-    var lcov = "";
-    Object.keys(global._$jscoverage).forEach(function(key) {
-        file = key;
-        lcov += "SF:" + file + "\n";
-        tmp = global._$jscoverage[key];
-        if (typeof tmp === 'function' || tmp.length === undefined) return;
-        total = touched = 0;
-        for (n = 0, len = tmp.length; n < len; n++) {
-            if (tmp[n] !== undefined) {
-                lcov += "DA:" + n + "," + tmp[n] + "\n";
-                total ++;
-                if (tmp[n] > 0)
-                    touched ++;
-            }
-        }
-        lcov += "end_of_record\n";
-        test.ok(touched); // Disable failures on this for now until full test suite is written
-        //test.equal(total, touched, 'All lines of code exercised by the tests');
+    var coverageStats = jscoverage.coverageStats();
+    Object.keys(coverageStats).forEach(function(file) {
+        test.equal(coverageStats[file].total, coverageStats[file].touched, 'All lines of code exercised by the tests');
     });
-    if(process.env.TRAVIS) coveralls.handleInput(lcov);
+    if(process.env.TRAVIS) coveralls.handleInput(jscoverage.getLCOV());
     test.done();
 };
